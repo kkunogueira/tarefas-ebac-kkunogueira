50c9259d0b88c34c049da1adb9770ced9740d34a
kkunogueira <kkunogueira2@hotmail.com>
2024-09-10 10:48:22 -0300

    TarefaCalculoMedia
    TarefaCalculoMedia

---
+++ Mod8/src/br/com/cacau/CalculoMedio.java
@@ -0,0 +1,15 @@
+package br.com.cacau;
+
+public class CalculoMedio {
+
+    public static void main(String args[]) {
+        int nota1 = 10;
+        int nota2 = 8;
+        int nota3 = 7;
+        int nota4 = 9;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(media);
+    }
+}
