82028651f04c0b9cf8f61371cb0fdcf0f7295533
kkunogueira <kkunogueira2@hotmail.com>
2024-09-11 10:41:38 -0300

    ExercicioMod9
    ExercicioMod9

---
+++ Mod9/src/br/com/cacau/Valor.java
@@ -0,0 +1,12 @@
+package br.com.cacau;
+
+public class Valor {
+    public static void  main(String args[]) {
+
+        Long num = 123456L;
+        System.out.println(num);
+
+        long num1 = Long.valueOf(456789);
+        System.out.println(num1);
+    }
+}
